10a3fa9660396cb3ef0a25b5f67cb7d8c39ffc1f
Alexey U <gr4k@bk.ru>
2025-08-02 18:34:11 +0300

    Draw vertical lines - frets separators

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use bevy::render::RenderPlugin;
 use bevy::render::render_asset::RenderAssetUsages;
 use bevy::render::settings::*;
 
-const NOTES: [&'static str; 7] = ["A", "B", "C", "D", "E", "F", "G"];
+const NOTES: [&'static str; 7] = ["A", "B", "aC", "D", "E", "F", "G"];
 const GAP: f32 = 100.0;
+const GREY: Color = Color::srgba(0.3, 0.3, 0.3, 1.0);
+const AMOUNT_OF_FRETS: u8 = 10;
 
 struct Tunning {
     name: &'static str,
@@ -64,15 +66,18 @@ fn setup(
     let window_width: f32 = window.width();
     let window_height: f32 = window.height();
 
-    // Рисуем горизонтальные линии "струны":
-    for i in 0..tunning.notes.len() {
-        let line_start: f32 = -window_width / 2.0 + GAP;
-        let line_end: f32 = window_width / 2.0 - GAP;
+    let line_start_x: f32 = -window_width / 2.0 + GAP;
+    let line_end_x: f32 = window_width / 2.0 - GAP;
 
-        // Координаты разделительной линии
-        let y_of_line: f32 = i as f32 * 40.0;
+    let mut last_str_y: Option<f32> = None;
+    // Рисуем горизонтальные линии - "струны":
+    for i in 0..tunning.notes.len() {
+        let y_of_line: f32 = i as f32 * GAP / 2.0;
+        if i == tunning.notes.len() - 1 {
+            last_str_y = Some(y_of_line);
+        }
         let vertices: Vec<[f32; 3]> =
-            vec![[line_start, y_of_line, 0.0], [line_end, y_of_line, 0.0]];
+            vec![[line_start_x, y_of_line, 0.0], [line_end_x, y_of_line, 0.0]];
         // Создаём mesh линий
         let mut line_mesh: Mesh = Mesh::new(
             bevy::render::mesh::PrimitiveTopology::LineStrip,
@@ -83,10 +88,9 @@ fn setup(
         commands.spawn((
             Mesh2d(meshes.add(line_mesh)),
             MeshMaterial2d(materials.add(Color::WHITE)),
-            Transform::from_xyz(0.0, 0.0, 0.0),
         ));
 
-        // Draw the NOTE
+        // Draw the NOTE name
         commands.spawn((
             Text2d::new(tunning.notes[i]),
             TextFont {
@@ -94,7 +98,29 @@ fn setup(
                 ..default()
             },
             TextColor(Color::WHITE),
-            Transform::from_xyz(line_start - GAP / 2.0, y_of_line, 0.0),
+            Transform::from_xyz(line_start_x - GAP / 2.0, y_of_line, 0.0),
         ));
+    }
+
+    if let Some(value) = last_str_y {
+        let line_end_y: f32 = value + GAP / 2.0;
+        // Рисуем вертикальные линии - "разделители ладов":
+        for i in 0..AMOUNT_OF_FRETS {
+            let line_start_y: f32 = -GAP / 2.0;
+            let x_of_line: f32 = line_start_x + i as f32 * GAP;
+            let vertices: Vec<[f32; 3]> =
+                vec![[x_of_line, line_start_y, 0.0], [x_of_line, line_end_y, 0.0]];
+            // Создаём mesh линий
+            let mut line_mesh: Mesh = Mesh::new(
+                bevy::render::mesh::PrimitiveTopology::LineStrip,
+                RenderAssetUsages::RENDER_WORLD,
+            );
+            line_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+            // Создаём Entity с этим Mesh
+            commands.spawn((
+                Mesh2d(meshes.add(line_mesh)),
+                MeshMaterial2d(materials.add(GREY)),
+            ));
+        }
     }
 }
